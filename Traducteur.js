b5dba9a03b2d9d9a629301ccfc57ab3be618982e
benjamindev34 <bennyben34@gmail.com>
2022-12-19 16:25:52 +0100

    mise en route du projet

--- Traducteur.js
+++ Traducteur.js
@@ -1,30 +1,57 @@
-let input = document.querySelector("#translator_input");
-word = input.value;
-console.log(word);
+// On récupère nos éléments
+const input = document.querySelector("#translator_input");
+const form = document.querySelector("form");
+
+// On crée une fonction pour gérer le submit
+const handleSubmit = function (event) {
+
+	// ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
+	event.preventDefault();
+	// hasOwnProperty vérifie si la propriété existe dans un objet
+	if(traduction.hasOwnProperty(input.value)){
+		console.log(traduction[input.value]);
+	}
+	else{
+		console.log("false");
+	}
+}
+// On rajoute l'event sur l'envoi du formulaire
+form.addEventListener("submit", handleSubmit);
 
 let francais = ["bonjour", "merci"];
 
 let alsacien = ["bùschùr", "merci vielmols"];
 
-if (word == francais[0]) {
-  console.log(alsacien[0]);
+let traduction = {
+	"bonjour-salut" : "bùschùr",
+	merci : "merci vielmols"
 }
 
+// if (word == francais[0]) {
+//   console.log(alsacien[0]);
+// }
+
+
+
+
+
+
 const boardPrononciation = {
-  init: function () {
-    boardPrononciation.btnPrononciation =
-      document.querySelector("#prononciation");
-    boardPrononciation.btnPrononciation.addEventListener(
-      "click",
-      boardPrononciation.handleClick
-    );
-    //console.log(boardPrononciation.btnPrononciation);
-  },
-  handleClick: function (event) {
-    event.preventDefault();
-    const asideElem = document.querySelector("ul");
-    asideElem.classList.toggle("list--hidden");
-  },
+	init: function () {
+		boardPrononciation.btnPrononciation =
+			document.querySelector("#prononciation");
+		boardPrononciation.btnPrononciation.addEventListener(
+			"click",
+			boardPrononciation.handleClick
+		);
+		//console.log(boardPrononciation.btnPrononciation);
+	},
+	handleClick: function (event) {
+		event.preventDefault();
+		const asideElem = document.querySelector("ul");
+		asideElem.classList.toggle("list--hidden");
+	},
+
 };
 
 boardPrononciation.init();
