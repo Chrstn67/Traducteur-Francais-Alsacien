a640d032bc1671007143456858476d182dd9d892
Chrstn67 <chrstn.hmbrt67@outlook.com>
2022-12-19 18:13:11 +0100

    gestion des majuscules, idée d'autocomplétion en cours, ajout des mots ou expressions

--- Traducteur.js
+++ Traducteur.js
@@ -1,15 +1,43 @@
-let input = document.querySelector("#translator_input");
-word = input.value;
-console.log(word);
+// On récupère nos éléments
+const form = document.querySelector("form");
+// On crée une fonction pour gérer le submit
+const handleSubmit = function (event) {
+  // ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
+  event.preventDefault();
 
-let francais = ["bonjour", "merci"];
+  let input = document.querySelector("#translator_input").value;
+  input = input.toLowerCase();
+  console.log(input);
 
-let alsacien = ["bùschùr", "merci vielmols"];
+  // hasOwnProperty vérifie si la propriété existe dans un objet
+  if (traduction.hasOwnProperty(input)) {
+    // console.log(traduction[input.value]);
 
-if (word == francais[0]) {
-  console.log(alsacien[0]);
-}
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = traduction[input];
+  } else {
+    // console.log("false");
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = "Mot ou expression inconnu(e) au bataillon...";
+  }
+};
 
+// On rajoute l'event sur l'envoi du formulaire
+form.addEventListener("submit", handleSubmit);
+
+let traduction = {
+  bonjour: "Bùschùr (Guete Morje)",
+  bonsoir: "Guete'n Owe",
+  salut: "Salü",
+  merci: "Merci",
+  "merci beaucoup": "Merci vielmols",
+  "bon après-midi": "Gueter middàà",
+  "au revoir": "O revoir",
+  "à tout de suite": "Bis glich",
+  "à toute à l'heure": "Bis noochhärde",
+};
+
+// Tableau prononciation
 const boardPrononciation = {
   init: function () {
     boardPrononciation.btnPrononciation =
@@ -32,16 +60,18 @@ boardPrononciation.init();
 // barre de recherche à adapter:
 // https://stacklima.com/barre-de-recherche-utilisant-html-css-et-javascript/
 
-// const search_word = function keyup() {
-//   let input = document.getElementById("translatorFrAls").value;
-//   input = input.toLowerCase();
-//   let x = document.getElementsByClassName("list-proposition");
+const search_word = function keyup() {
+  let input = document.getElementById("translator_input").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("list-proposition");
 
-//   for (i = 0; i < x.length; i++) {
-//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//       x[i].style.display = "none";
-//     } else {
-//       x[i].style.display = "list-item";
-//     }
-//   }
-// };
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+};
+
+search_word();
