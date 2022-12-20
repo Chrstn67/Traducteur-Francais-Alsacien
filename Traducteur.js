ec041d9f6f6a80abf41472da5776c50e8b5b35ef
Chrstn67 <113202226+Chrstn67@users.noreply.github.com>
2022-12-20 17:38:34 +0100

    Update Traducteur.js

--- Traducteur.js
+++ Traducteur.js
@@ -1,35 +1,11 @@
-// On récupère nos éléments
-const form = document.querySelector("form");
-// On crée une fonction pour gérer le submit
-const handleSubmit = function (event) {
-  // ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
-  event.preventDefault();
-
-  let input = document.querySelector("#translator_input").value;
-  input = input.toLowerCase();
-  console.log(input);
-
-  // hasOwnProperty vérifie si la propriété existe dans un objet
-  if (traduction.hasOwnProperty(input)) {
-    // console.log(traduction[input.value]);
-
-    const result = document.querySelector("#resultAls");
-    result.innerHTML = traduction[input];
-  } else {
-    // console.log("false");
-    const result = document.querySelector("#resultAls");
-    result.innerHTML = "Mot ou expression inconnu(e) au bataillon...";
-  }
-};
-
-// On rajoute l'event sur l'envoi du formulaire
-form.addEventListener("submit", handleSubmit);
-
 const traduction = {
   bonjour: "Bùschùr (Guete Morje)",
   bonsoir: "Guete'n Owe",
   salut: "Salü",
   merci: "Merci",
+  prénom: "Vornàmme",
+  nom: "Nàmme",
+  "nom de famille": "Fàmilienàmme",
   "merci beaucoup": "Merci vielmols",
   "bon après-midi": "Gueter middàà",
   "au revoir": "O revoir",
@@ -40,7 +16,42 @@ const traduction = {
   "bonne journée": "Gueter Dàà",
   "bonne nuit": "Guet Nàcht",
   "tout le monde": "bisàmme",
+  "faire connaissance": "sich kännelehere",
+  "comment t'appelles-tu ?": "Wie heisch dü ?",
+  "je m'appelle...": "Ìch heiss...",
+  "et toi ?": "ùn dü ?",
+  "quel est ton nom ?": "Wie ìsch diner Nàmme ?",
+  "quel est votre prénom ?": "Wie ìsch diner Vornàmme ?",
+  "mon nom est...": "Minner nàmme ìsch",
+  "mon prénom est...": "Minner vornàmmer ìsch",
 };
+
+// On crée une fonction pour gérer le submit
+const handleSubmit = function (event) {
+  // ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
+  event.preventDefault();
+  let input;
+  input = document.querySelector("#translator_input").value;
+  input = input.toLowerCase();
+  console.log(input);
+  if (input === "") {
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = "Veuillez entrer ce que vous souhaitez traduire";
+  }
+  // hasOwnProperty vérifie si la propriété existe dans un objet
+  else if (traduction.hasOwnProperty(input)) {
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = traduction[input];
+  } else {
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = "Mot ou expression inconnu(e) au bataillon...";
+  }
+};
+
+// On récupère nos éléments
+const form = document.querySelector("form");
+// On rajoute l'event sur l'envoi du formulaire
+form.addEventListener("submit", handleSubmit);
 
 // tu récupères la section
 const section = document.querySelector("#filtre");
@@ -48,20 +59,23 @@ const section = document.querySelector("#filtre");
 const displayTranslations = (translations) => {
   // tu vides la section
   section.innerHTML = "";
+  let i = 0;
   // tu boucles sur les clés du tableau et tu affiches les traductions
   for (const key in translations) {
     const div = document.createElement("div");
     div.textContent = `${key} : ${traduction[key]}`;
     section.appendChild(div);
+    i = i + 1;
+    if (i === 7) {
+      break;
+    }
   }
 };
 
 // tu appelles la fonction pour afficher les traductions
-displayTranslations(traduction);
 
 // tu récupères le champ de recherche
 const input = document.querySelector("input");
-// input = input.toLowerCase();
 // tu écoutes l'événement input
 input.addEventListener("input", (event) => {
   // tu récupères la valeur de l'input
@@ -75,7 +89,6 @@ input.addEventListener("input", (event) => {
       delete filtered[key];
     }
   }
-
   // tu utilises la fonction
   displayTranslations(filtered);
 });
@@ -89,7 +102,6 @@ const boardPrononciation = {
       "click",
       boardPrononciation.handleClick
     );
-    //console.log(boardPrononciation.btnPrononciation);
   },
   handleClick: function (event) {
     event.preventDefault();
@@ -100,21 +112,17 @@ const boardPrononciation = {
 
 boardPrononciation.init();
 
-// // barre de recherche à adapter:
-// // https://stacklima.com/barre-de-recherche-utilisant-html-css-et-javascript/
+// Bouton infos
+const btnInfos = {
+  init: function () {
+    btnInfos.btnPrononciation = document.querySelector(".infos");
+    btnInfos.btnPrononciation.addEventListener("click", btnInfos.handleClick);
+  },
+  handleClick: function (event) {
+    event.preventDefault();
+    const pElem = document.querySelector("p");
+    pElem.classList.toggle("infos-Als--hidden");
+  },
+};
 
-// const search_word = function keyup() {
-//   let input = document.getElementById("translator_input").value;
-//   input = input.toLowerCase();
-//   let x = document.getElementsByClassName("list-proposition");
-
-//   for (i = 0; i < x.length; i++) {
-//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//       x[i].style.display = "none";
-//     } else {
-//       x[i].style.display = "list-item";
-//     }
-//   }
-// };
-
-// search_word();
+btnInfos.init();
