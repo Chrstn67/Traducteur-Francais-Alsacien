6c26b9da3a12382b88bcb9b6bda8f0d2759c5053
Chrstn67 <113202226+Chrstn67@users.noreply.github.com>
2022-12-20 10:19:10 +0100

    Update Traducteur.js

--- Traducteur.js
+++ Traducteur.js
@@ -1,57 +1,58 @@
 // On récupère nos éléments
-const input = document.querySelector("#translator_input");
 const form = document.querySelector("form");
-
 // On crée une fonction pour gérer le submit
 const handleSubmit = function (event) {
+  // ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
+  event.preventDefault();
 
-	// ATTENTION PREVENT DEFAULT ULTRA IMPORTANT SINON RECHARGEMENT DE PAGE
-	event.preventDefault();
-	// hasOwnProperty vérifie si la propriété existe dans un objet
-	if(traduction.hasOwnProperty(input.value)){
-		console.log(traduction[input.value]);
-	}
-	else{
-		console.log("false");
-	}
-}
+  let input = document.querySelector("#translator_input").value;
+  input = input.toLowerCase();
+  console.log(input);
+
+  // hasOwnProperty vérifie si la propriété existe dans un objet
+  if (traduction.hasOwnProperty(input)) {
+    // console.log(traduction[input.value]);
+
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = traduction[input];
+  } else {
+    // console.log("false");
+    const result = document.querySelector("#resultAls");
+    result.innerHTML = "Mot ou expression inconnu(e) au bataillon...";
+  }
+};
+
 // On rajoute l'event sur l'envoi du formulaire
 form.addEventListener("submit", handleSubmit);
 
-let francais = ["bonjour", "merci"];
-
-let alsacien = ["bùschùr", "merci vielmols"];
-
 let traduction = {
-	"bonjour-salut" : "bùschùr",
-	merci : "merci vielmols"
-}
+  bonjour: "Bùschùr (Guete Morje)",
+  bonsoir: "Guete'n Owe",
+  salut: "Salü",
+  merci: "Merci",
+  "merci beaucoup": "Merci vielmols",
+  "bon après-midi": "Gueter middàà",
+  "au revoir": "O revoir",
+  "à tout de suite": "Bis glich",
+  "à toute à l'heure": "Bis noochhärde",
+};
 
-// if (word == francais[0]) {
-//   console.log(alsacien[0]);
-// }
-
-
-
-
-
-
+// Tableau prononciation
 const boardPrononciation = {
-	init: function () {
-		boardPrononciation.btnPrononciation =
-			document.querySelector("#prononciation");
-		boardPrononciation.btnPrononciation.addEventListener(
-			"click",
-			boardPrononciation.handleClick
-		);
-		//console.log(boardPrononciation.btnPrononciation);
-	},
-	handleClick: function (event) {
-		event.preventDefault();
-		const asideElem = document.querySelector("ul");
-		asideElem.classList.toggle("list--hidden");
-	},
-
+  init: function () {
+    boardPrononciation.btnPrononciation =
+      document.querySelector("#prononciation");
+    boardPrononciation.btnPrononciation.addEventListener(
+      "click",
+      boardPrononciation.handleClick
+    );
+    //console.log(boardPrononciation.btnPrononciation);
+  },
+  handleClick: function (event) {
+    event.preventDefault();
+    const asideElem = document.querySelector("ul");
+    asideElem.classList.toggle("list--hidden");
+  },
 };
 
 boardPrononciation.init();
@@ -59,16 +60,19 @@ boardPrononciation.init();
 // barre de recherche à adapter:
 // https://stacklima.com/barre-de-recherche-utilisant-html-css-et-javascript/
 
-// const search_word = function keyup() {
-//   let input = document.getElementById("translatorFrAls").value;
-//   input = input.toLowerCase();
-//   let x = document.getElementsByClassName("list-proposition");
+const search_word = function keyup() {
+  let input = document.getElementById("translator_input").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("list-proposition");
 
-//   for (i = 0; i < x.length; i++) {
-//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//       x[i].style.display = "none";
-//     } else {
-//       x[i].style.display = "list-item";
-//     }
-//   }
-// };
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+};
+
+search_word();
+
